87fbfdb8471099576d1a1a46f39b6d7381a419ec
Cadell Christo <git@cadell.dev>
2022-02-16 07:40:17 +0800

    run prettier on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,4 @@
-import {
-	Container,
-	MdxProvider,
-	Header,
-} from "cadells-vanilla-components";
+import { Container, MdxProvider, Header } from "cadells-vanilla-components";
 import "cadells-vanilla-components/dist/index.css";
 import "@fontsource/source-sans-pro/400.css";
 import "@fontsource/source-sans-pro/600.css";
@@ -10,7 +6,7 @@ import "@fontsource/source-sans-pro/600.css";
 const App = ({ Component, pageProps }) => (
 	<MdxProvider>
 		<Container>
-			<Header githubHref="https://github.com/cadbox1/cadells-nextjs-template"/>
+			<Header githubHref="https://github.com/cadbox1/cadells-nextjs-template" />
 			<Component {...pageProps} />
 		</Container>
 	</MdxProvider>
